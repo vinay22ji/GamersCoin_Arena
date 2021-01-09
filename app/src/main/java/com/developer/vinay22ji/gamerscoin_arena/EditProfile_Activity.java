2f6559edb06f01cc86d27d0e347171cbe3064c7c
vinay <vinay22ji2@gmail.com>
2021-01-09 18:01:44 +0530

    Edit adtivity added

---
+++ app/src/main/java/com/developer/vinay22ji/gamerscoin_arena/EditProfile_Activity.java
@@ -0,0 +1,14 @@
+package com.developer.vinay22ji.gamerscoin_arena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProfile_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_);
+    }
+}
